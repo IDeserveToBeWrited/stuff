5526d74534035278e20edd7f8cb0286523f93f22
IDeserveToBeWrited <dodo.coowbooy@gmail.com>
2021-10-24 16:26:03 +0200

    fixed bugs and overall useability

--- deep cock galactic/mp3.bat
+++ deep cock galactic/mp3.bat
@@ -4,4 +4,4 @@ REM transcode to mp3
 for /r "C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Content\Paks\Nowy folder2\FSD\Content\Audio\" %%a in (.) do (FOR %%e IN ("%%~da%%~pa%%~na\*.OGG") DO "ffmpeg.exe" -i "%%e" -acodec libmp3lame -q:a 0 -y "%%~de%%~pe%%~ne.mp3")
 
 REM delete OGG
-for /r "C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Content\Paks\Nowy folder2\FSD\Content\Audio\" %%b in (.) do (FOR %%f IN ("%%~db%%~pb%%~nb\*.OGG") DO del %%f >NUL)
+for /r "C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Content\Paks\Nowy folder2\FSD\Content\Audio\" %%b in (.) do (FOR %%f IN ("%%~db%%~pb%%~nb\*.OGG") DO del "%%f" >NUL)
