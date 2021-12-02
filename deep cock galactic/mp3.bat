a7f3260cc25214d23a10f192b8d460dc61d2ebcf
IDeserveToBeWrited <dodo.coowbooy@gmail.com>
2021-12-02 20:46:47 +0100

    naprawiono naprawienie

--- deep cock galactic/mp3.bat
+++ deep cock galactic/mp3.bat
@@ -7,7 +7,7 @@ SET /A CURRENT=1
 for /r "C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Content\Paks\Nowy folder2\FSD\Content\Audio\" %%c in (.) do (FOR %%g IN ("%%~dc%%~pc%%~nc\*.OGG") DO SET /A COUNT=COUNT+1)
 
 REM transcode to mp3 / https://superuser.com/questions/326629/how-can-i-make-ffmpeg-be-quieter-less-verbose
-for /r "C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Content\Paks\Nowy folder2\FSD\Content\Audio\" %%a in (.) do (FOR %%e IN ("%%~da%%~pa%%~na\*.OGG") DO (echo ^(!CURRENT!\%COUNT%^) & "ffmpeg.exe" -hide_banner -loglevel error -i "%%e" -acodec libmp3lame -q:a 0 -y "%%~de%%~pe%%~ne.mp3" & SET /A CURRENT+=1))
+for /r "C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Content\Paks\Nowy folder2\FSD\Content\Audio\" %%a in (.) do (FOR %%e IN ("%%~da%%~pa%%~na\*.ogg") DO (echo mp3 ^(!CURRENT!\%COUNT%^) & "ffmpeg.exe" -hide_banner -loglevel error -i "%%e" -acodec libmp3lame -q:a 0 -y "%%~de%%~pe%%~ne.mp3" & SET /A CURRENT+=1))
 
 REM delete OGG
 ECHO del
